6698c2bef7d5a1f35dfb7c6c661ee69794303b7d
peter.s.gu@gmail.com <peter.s.gu@gmail.com@d2acdd72-1bf2-11df-acd4-e1cde50d4098>
2010-03-03 19:09:39 +0000

    Phyiscs update: jumping, walking. Physics thread is synchronized with the GLCanvas, which has the keylistener now

--- branches/physics/FrameRunner.java
+++ branches/physics/FrameRunner.java
@@ -8,7 +8,7 @@ public class FrameRunner extends JFrame {
 	private static GameWindow game;
 	public FrameRunner () {
 		game= new GameWindow();
-		setSize(400,400);
+		setSize(800,800);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	public static void main(String[] args) {
