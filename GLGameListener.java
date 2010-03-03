050641bf0a8f8dfd514d1e8ec3b53fce179d14cd
peter.s.gu@gmail.com <peter.s.gu@gmail.com@d2acdd72-1bf2-11df-acd4-e1cde50d4098>
2010-03-03 19:09:39 +0000

    Phyiscs update: jumping, walking. Physics thread is synchronized with the GLCanvas, which has the keylistener now

--- GLGameListener.java
+++ GLGameListener.java
@@ -1,11 +1,22 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.media.opengl.GL;
 import javax.media.opengl.GLAutoDrawable;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
 
-public class GLGameListener implements GLEventListener {
-
+public class GLGameListener implements GLEventListener, KeyListener  {
+	Object sync;
+	Player player;
+	boolean up;
+	boolean left;
+	boolean right;
+	public GLGameListener(Player you, Object synch) {
+		player=you;
+		sync=synch;
+	}
 	public void display(GLAutoDrawable drawable) {
 		GL gl = drawable.getGL();   		
 		GLU glu = new GLU(); 	
@@ -14,7 +25,7 @@ public class GLGameListener implements GLEventListener {
 	 	gl.glLoadIdentity();		 			 	
 	 	glu.gluPerspective(60,1.0,1.0,100);
 	 	gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
-	 	glu.gluLookAt(0,0,5,0,0,0,0,1,0);
+	 	glu.gluLookAt(0,0,50,0,0,0,0,1,0);
 	 	gl.glMatrixMode(GL.GL_MODELVIEW);
 	 	gl.glBegin(GL.GL_LINES);
 	 		gl.glVertex3d(0,-50,0);
@@ -27,15 +38,25 @@ public class GLGameListener implements GLEventListener {
 			gl.glVertex3d(0,0,-9001);
 			gl.glVertex3d(0,0,9001);	 			
 		gl.glEnd();
+		gl.glBegin(GL.GL_QUADS);
+			gl.glVertex3d(player.x-1,player.y-1,0);
+			gl.glVertex3d(player.x+1,player.y-1,0);
+			gl.glVertex3d(player.x+1,player.y+1,0);
+			gl.glVertex3d(player.x-1,player.y+1,0);
+		gl.glEnd();
+		//****************KEY RESPONSE*******************
+		if(left) 
+			player.vx=-.3;
+		if(right)
+			player.vx=.3;
+		if(up)
+			player.vy=1;
 	}
 
-	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {
-		// TODO Auto-generated method stub
-		
+	public void displayChanged(GLAutoDrawable arg0, boolean arg1, boolean arg2) {		
 	}
 
 	public void init(GLAutoDrawable drawable) {
-		System.out.println("hi");
 		GL gl = drawable.getGL();   		
 		GLU glu = new GLU(); 	
 		gl.glClearColor(0.0f,0.0f,0.0f,0.0f);
@@ -44,10 +65,29 @@ public class GLGameListener implements GLEventListener {
 	 	glu.gluPerspective(60,1.0,1.0,100);
 	}
 
-	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3,
-			int arg4) {
-		// TODO Auto-generated method stub
+	public void reshape(GLAutoDrawable arg0, int arg1, int arg2, int arg3, int arg4) {
 		
 	}
-
+	
+	//**************KEY LISTENER********************
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT)
+			left=true;
+		else if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+			right=true;
+		else if(e.getKeyCode()==KeyEvent.VK_UP)
+			up=true;
+				
+	}
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()==KeyEvent.VK_LEFT)
+			left=false;
+		else if(e.getKeyCode()==KeyEvent.VK_RIGHT)
+			right=false;
+		else if(e.getKeyCode()==KeyEvent.VK_UP)
+			up=false;
+	}
+	public void keyTyped(KeyEvent e) {
+	}
+	
 }
--- GameWindow.java
+++ GameWindow.java
@@ -9,21 +9,20 @@ import javax.swing.SwingUtilities;
 
 import com.sun.opengl.util.Animator;
 
-public class GameWindow extends JPanel implements KeyListener {
-	private int width;
-	private int height;
+public class GameWindow extends JPanel {
+	Object sync= new Object();
 	Player player;
 	private PhysicsThread physics;
 	Animator anim;
 	public GameWindow() {
-		super(new BorderLayout());
-		width= 400;
-		height= 400;		
+		super(new BorderLayout());	
 		//setSize(width,height);
 		player= new Player();
 		GLCapabilities glcaps = new GLCapabilities(); 
 		GLCanvas glCanvas = new GLCanvas(glcaps);
-		GLGameListener listener= new GLGameListener();
+
+		GLGameListener listener= new GLGameListener(player,sync);
+		glCanvas.addKeyListener(listener);
 		glCanvas.addGLEventListener(listener);   // add event listener
 		add(glCanvas);
 		physics= new PhysicsThread();
@@ -31,37 +30,45 @@ public class GameWindow extends JPanel implements KeyListener {
 		anim=new Animator(glCanvas);
 		anim.start();
 	}
-	public void keyPressed(KeyEvent e) {	
-		if(e.getKeyChar()==KeyEvent.VK_LEFT)
-			player.ax=-10;
-		else if(e.getKeyChar()==KeyEvent.VK_RIGHT)
-			player.ax=10;
-	}
-	public void keyReleased(KeyEvent e) {
-	}
-	public void keyTyped(KeyEvent e) {
-	}
+	//***************************PHYSICS THREAD***********************
 	public class PhysicsThread extends Thread {
-		private double muk;
-		private double gravity=9.8;
+		private double muk=.1;
+		private double gravity=9.8/100;
 		private long dt;
 		private long time;
-		public PhysicsThread() {
+		public PhysicsThread() {			
 			dt=1;
 		}
 		public void run() {
-
-			if(player.y<0) {
-				player.ay=0;
-			}
-			player.vy+=player.ay*dt;
-			player.vx+=player.ax*dt;
-			player.y+=player.vy;
-			player.x+=player.vx;
-			if(player.ax>0 && player.ax+muk*player.m*gravity>0)
-				player.ax-=muk*player.m*gravity;
-			else if(player.ax<0 && player.ax+muk*player.m*gravity<0)
-				player.ax+=muk*player.m*gravity;
+			while(true) {
+				synchronized (sync) {
+					//ground friction
+					if(player.vx>0) {
+						player.ax= Math.max(-player.vx/dt,-muk*gravity);
+					} else if(player.vx<0) {
+						player.ax= Math.min(-player.vx/dt,muk*gravity);
+						//player.ax= player.ax+muk*gravity;
+					}
+					//gravity and ground detection
+					if(player.y<=0 && player.vy<=0) {
+						player.ay=0;
+						player.vy=0;
+					} else
+						player.ay=-gravity;
+					player.vy+=player.ay*dt;
+					player.vx+=player.ax*dt;
+					if(player.y+player.vy*dt<0)
+						player.y=0;
+					else 
+						player.y+=player.vy*dt;
+					player.x+=player.vx*dt;
+					player.ax=0;
+					player.ay=0;
+				}
+				try {				
+					Thread.sleep(33L);
+				} catch (InterruptedException e) {}
+			}			
 		}
 		
 	}
