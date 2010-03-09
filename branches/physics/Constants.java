a2dba06ea54630c304c8707b603fda7c860a9504
peter.s.gu@gmail.com <peter.s.gu@gmail.com@d2acdd72-1bf2-11df-acd4-e1cde50d4098>
2010-03-09 19:10:20 +0000

    3/9 Direction of user is shown, duck jumping, constants for user state

---
+++ branches/physics/Constants.java
@@ -0,0 +1,15 @@
+/**
+ * Instead of using strings to indicate a property of the player, each possible state is linked to a number.
+ *
+ */
+public interface Constants {
+	/* Position variable indicates the player's state:
+	 * 0: standing
+	 * 1: ducking
+	 * 2: airborne
+	 * 3: on a ladder
+	 */
+	public static final int STANDING=0;
+	public static final int DUCKING=1;
+	public static final int AIRBORNE=2;
+}
---
+++ branches/physics/Dive.java
@@ -0,0 +1,10 @@
+/**
+ * A quick forward tumble
+ */
+public class Dive extends Move {
+	public Dive() {
+		start=4;
+		end=25;
+		duration=29;
+	}
+}
